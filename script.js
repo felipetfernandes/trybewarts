1da584c63c91a4f233f1ae0790b2d5a65af04f4a
felipetfernandes <felipe.tavares.fernandes@gmail.com>
2022-10-14 18:24:14 -0300

    21 in progress 2

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const loginButton = document.getElementById('login');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const textArea = document.querySelector('textarea');
-const counter = document.getElementById('counter');
-const getRequiredCheckbox = document.getElementById('agreement');
-const buttonSubmmit = document.getElementById('submit-btn');
-const form = document.getElementById('evaluation-form');
-const formData = document.getElementById('form-data');
-const name = document.getElementById('input-name');
-const lastName = document.getElementById('input-lastname');
-const newEmail = document.getElementById('input-email');
-const house = document.getElementById('house');
-const family = document.getElementById('label-family');
+const loginButton = document.getElementById("login");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const textArea = document.querySelector("textarea");
+const counter = document.getElementById("counter");
+const getRequiredCheckbox = document.getElementById("agreement");
+const buttonSubmmit = document.getElementById("submit-btn");
+const form = document.getElementById("evaluation-form");
+const formData = document.getElementById("form-data");
+const name = document.getElementById("input-name");
+const lastName = document.getElementById("input-lastname");
+const newEmail = document.getElementById("input-email");
+const house = document.getElementById("house");
+const family = document.getElementById("label-family");
+const techList = document.getElementsByClassName("subject");
 
 const login = () => {
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
-    window.alert('Olá, Tryber!');
+  if (email.value === "tryber@teste.com" && password.value === "123456") {
+    window.alert("Olá, Tryber!");
   } else {
-    window.alert('Email ou senha inválidos.');
+    window.alert("Email ou senha inválidos.");
   }
 };
 
@@ -34,24 +35,36 @@ function enableButton() {
   }
 }
 
+function techs() {
+  let lista = '';
+  for (let index = 0; index < techList.length; index += 1) {
+    if (techList[index].checked) {
+      lista += `${techList[index].value}, `;
+    }
+  }
+  lista = lista.slice(0, lista.length - 2);
+  return lista;
+}
+
 function criarLinha(input, text) {
-    const linha = document.createElement('p');
-    linha.innerText = `${input}: ${text}`;
-    formData.appendChild(linha);
+  const linha = document.createElement("p");
+  linha.innerText = `${input}: ${text}`;
+  formData.appendChild(linha);
 }
 
 function formChange() {
-  form.style.display = 'none';
-  formData.classList.add('form-data');
-  formData.style.display = 'flex';
-  criarLinha('Nome', `${name.value} ${lastName.value}`);
-  criarLinha('Email', newEmail.value);
-  criarLinha('Casa', house.value);
-  criarLinha('Família', family.value);
+  form.style.display = "none";
+  formData.classList.add("form-data");
+  formData.style.display = "flex";
+  criarLinha("Nome", `${name.value} ${lastName.value}`);
+  criarLinha("Email", newEmail.value);
+  criarLinha("Casa", house.value);
+  criarLinha("Família", family.value);
+  criarLinha("Matérias", techs());
   event.preventDefault();
 }
 
-getRequiredCheckbox.addEventListener('click', enableButton);
-loginButton.addEventListener('click', login);
-textArea.addEventListener('input', count);
-buttonSubmmit.addEventListener('click', formChange);
+getRequiredCheckbox.addEventListener("click", enableButton);
+loginButton.addEventListener("click", login);
+textArea.addEventListener("input", count);
+buttonSubmmit.addEventListener("click", formChange);
