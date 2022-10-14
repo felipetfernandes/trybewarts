586622811717cab546c0a54b54665c932bf8aff8
felipetfernandes <felipe.tavares.fernandes@gmail.com>
2022-10-14 16:54:47 -0300

    request 21 done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const loginButton = document.getElementById('login');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const textArea = document.querySelector('textarea');
+const counter = document.getElementById('counter');
 
 const login = () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -10,4 +12,9 @@ const login = () => {
   }
 };
 
+const count = () => {
+  counter.innerText = (500 - textArea.value.length);
+};
+
 loginButton.addEventListener('click', login);
+textArea.addEventListener('input', count);
